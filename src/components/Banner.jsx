4002fe0335022899c72f3ee4766e6fac615cc00a
lu_mbp <48710248+agwurn@users.noreply.github.com>
2023-10-26 14:04:49 +0800

    v0.0.3 build mobile Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,9 +3,10 @@
 
 export default function Banner() {
   return (
-    <div className='bg-contain bg-yellow-100 h-full'>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam molestiae facilis laudantium eius sed earum omnis consectetur rem voluptates, at placeat reiciendis! Atque, minus sint cupiditate sapiente vel dolor veniam.
+    <div className='bg-cover bg-banner bg-center bg-s h-[311px] p-4 flex flex-col justify-end items-end'>
+      <h1 className="w-2/3 text-right text-5xl leading-[50px] text-custom-white font-bold">白頭翁 (Chinese bulbul)</h1>
+      <div className="mt-4 text-[18px] text-custom-white">
+        又名白頭鵯。以果實、昆蟲為主食，無法消化小米、穀類。平均壽命約 8~10 年。
       </div>
     </div>
   )
